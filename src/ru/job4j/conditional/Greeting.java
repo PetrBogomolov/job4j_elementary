2e1ea9462ef8d9625dc14c300dc59f4f32ff389a
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-09-24 14:04:07 +0300

    9.1. String [#371558]

---
+++ src/ru/job4j/conditional/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j.conditional;
+
+public class Greeting {
+    public static void main(String[] args) {
+
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String addString = "But i am a newbie.";
+        int year = 2020;
+        idea = idea + " " + addString + " " + year;
+        System.out.println(idea);
+
+    }
+}
