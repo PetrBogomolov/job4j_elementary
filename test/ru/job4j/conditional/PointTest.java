3b99474871012ad26e21ce92a4bca6c64fc9e44a
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-09-25 12:55:45 +0300

    4. Тесты для стороны прямоугольника. [#371583]

--- test/ru/job4j/conditional/PointTest.java
+++ test/ru/job4j/conditional/PointTest.java
@@ -28,34 +28,34 @@ public class PointTest {
     }
 
     @Test
-    public void when44toMinus3Minus4then10Comma63014581273465() {
+    public void when44toMinus3Minus4then10Comma63() {
         int x1 = 4;
         int y1 = 4;
         int x2 = -3;
         int y2 = -4;
-        double expected = 10.63014581273465;
+        double expected = 10.63;
         double out = Point.distance(x1, y1, x2, y2);
         assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void whenMinus1And3to3Minus3then7Comma211102550927978() {
+    public void whenMinus1And3to3Minus3then7Comma21() {
         int x1 = -1;
         int y1 = 3;
         int x2 = 3;
         int y2 = -3;
-        double expected = 7.211102550927978;
+        double expected = 7.21;
         double out = Point.distance(x1, y1, x2, y2);
         assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void whenMinus2Minus3toMinus4Minus1then2Comma8284271247461903() {
+    public void whenMinus2Minus3toMinus4Minus1then2Comma82() {
         int x1 = -2;
         int y1 = -3;
         int x2 = -4;
         int y2 = -1;
-        double expected = 2.8284271247461903;
+        double expected = 2.82;
         double out = Point.distance(x1, y1, x2, y2);
         assertEquals(expected, out, 0.01);
     }
