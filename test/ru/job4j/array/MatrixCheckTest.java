80c2f7d9239fb2b74ae4bf3ebfac29acc008897e
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-10-02 18:19:08 +0300

    6.7.2. Моно столбец в матрице. [#371527]

--- test/ru/job4j/array/MatrixCheckTest.java
+++ test/ru/job4j/array/MatrixCheckTest.java
@@ -18,13 +18,35 @@ public class MatrixCheckTest {
     }
 
     @Test
-    public void whenArrayHasNotSimilarElementsInLineThenTrue() {
+    public void whenArrayHasNotSimilarElementsInLineThenFalse() {
         char[][] input = {
                 {' ', ' ', ' '},
                 {'X', 'X', ' '},
                 {' ', ' ', ' '},
         };
         boolean result = MatrixCheck.monoHorizontal(input, 1);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void whenArrayHasSimilarElementsInColumnThenTrue() {
+        char[][] input = {
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+        };
+        boolean result = MatrixCheck.monoVertical(input, 2);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void whenArrayHasNotSimilarElementsInColumnThenFalse() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {' ', ' ', 'X'},
+                {' ', ' ', ' '},
+        };
+        boolean result = MatrixCheck.monoVertical(input, 2);
         assertEquals(false, result);
     }
 }
