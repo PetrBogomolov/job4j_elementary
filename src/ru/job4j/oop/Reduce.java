ccc42f058ad2b192c3c83bb57621a936d00bc586
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-10-06 18:17:22 +0300

    5.1. Зона видимости переменных [#371715]

--- src/ru/job4j/oop/Reduce.java
+++ src/ru/job4j/oop/Reduce.java
@@ -4,19 +4,18 @@ public class Reduce {
     private int[] array;
 
     public void to(int[] input) {
-        array = input;
+        this.array = input;
     }
 
     public void print() {
-        for (int index = 0; index < array.length; index++) {
-            System.out.println(array[index]);
+        for (int i : array) {
+            System.out.print(i);
         }
     }
 
     public static void main(String[] args) {
-        int[] input = {1, 2, 3};
         Reduce reduce = new Reduce();
-        reduce.to(input);
+        reduce.to(new int[] {1, 2, 3});
         reduce.print();
     }
 }
