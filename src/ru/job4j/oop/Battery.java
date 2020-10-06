f97a20b4d2b45b403775b5fab9f280d4743e6912
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-10-06 13:53:43 +0300

    1.7. Состояние объекта [#371713]

---
+++ src/ru/job4j/oop/Battery.java
@@ -0,0 +1,21 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        this.load = this.load - another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery full = new Battery(100);
+        Battery quarter = new Battery(25);
+        full.exchange(quarter);
+        System.out.println(full.load);
+    }
+}
