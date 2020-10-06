a220aaed86be97e8d6c63638c76750356e525548
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-10-06 17:55:17 +0300

    2.2. Рефакторинг - Площадь треугольника. [#371704]

--- src/ru/job4j/conditional/Triangle.java
+++ src/ru/job4j/conditional/Triangle.java
@@ -1,7 +1,37 @@
 package ru.job4j.conditional;
 
 public class Triangle {
-    public static boolean exist(double ab, double ac, double bc) {
+    private final Point first;
+    private final Point second;
+    private final Point third;
+
+    public Triangle(Point a, Point b, Point c) {
+        this.first = a;
+        this.second = b;
+        this.third = c;
+    }
+
+    public double period(double ab, double bc, double ac) {
+        return (ab + bc + ac) / 2;
+    }
+
+    public  boolean exist(double ab, double ac, double bc) {
         return ab + bc > ac && bc + ac > ab && ab + ac > bc;
+    }
+
+    public double area() {
+        double result = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        double p = period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            double stage1 = p - ab;
+            double stage2 = p - bc;
+            double stage3 = p - ac;
+            double stage4 = p * stage1 * stage2 * stage3;
+            result = Math.sqrt(stage4);
+        }
+        return result;
     }
 }
--- test/ru/job4j/conditional/TriangleTest.java
+++ test/ru/job4j/conditional/TriangleTest.java
@@ -5,16 +5,14 @@ import static org.junit.Assert.*;
 
 public class TriangleTest {
 
-    @Test
-    public void whenTriangleExist() {
-        boolean result = Triangle.exist(4.0, 4.0, 4.0);
-        assertEquals(true, result);
-    }
-
-    @Test
-    public void whenTriangleNoExist() {
-        boolean result = Triangle.exist(4.0, 4.0, 9.0);
-        assertEquals(false, result);
-
-    }
+   @Test
+   public void areaOfTriangle() {
+       Point a = new Point(0, 0);
+       Point b = new Point(4, 0);
+       Point c = new Point(0, 4);
+       Triangle triangle = new Triangle(a, b, c);
+       double result = triangle.area();
+       double expect = 8;
+       assertEquals(expect, result, 0.001);
+   }
 }
